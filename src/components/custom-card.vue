936731412c89db111de8ba8a2cbf8a507222df72
shani kupiec <shani.kupiec@gmail.com>
2022-07-25 15:03:21 +0300

    dashboard css

--- src/components/custom-card.vue
+++ src/components/custom-card.vue
@@ -1,6 +1,13 @@
 <template>
-  <article class="custom-card">
-    <slot name="title"></slot>
-    <slot name="data"></slot>
-  </article>
+  <section class="custom-card">
+    <slot name="img" class="custom-card__img"></slot>
+    <main class="custom-card__main">
+      <span class="custom-card__main__title">
+        <slot name="title"></slot>
+      </span>
+      <span>
+        <slot name="data"></slot>
+      </span>
+    </main>
+  </section>
 </template>
--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,9 +1,31 @@
 <template>
-  <section>
-    <customCard>
-      <template v-slot:title> date </template>
-      <template v-slot:data> 2022 </template>
-    </customCard>
+  <section class="dashboard">
+    <header class="dashboard__header">אזור אישי</header>
+    <main class="dashboard__main">
+      <customCard>
+        <template v-slot:img> <img src="../../public/Union.png" alt="" /> </template>
+        <template v-slot:title> תאריך:</template>
+        <template v-slot:data> 2022 </template>
+      </customCard>
+      <customCard>
+        <template v-slot:img> <img src="../../public/logout.png" alt="" /> </template>
+        <template v-slot:title> נכנס ב- </template>
+        <template v-slot:data> 2022 </template>
+      </customCard>
+      <customCard>
+        <template v-slot:img> <img src="../../public/time.png" alt="" /> </template>
+        <template v-slot:title> סה"כ שעות: </template>
+        <template v-slot:data> 2022 </template>
+      </customCard>
+      <customCard>
+        <template v-slot:img> <img src="../../public/coffee.png" alt="" /> </template>
+        <template v-slot:title> הפסקה: </template>
+        <template v-slot:data> 2022 </template>
+      </customCard>
+      <button class="dashboard__main__btn" style="backgroundColor:#5863F8">כניסה למשמרת</button>
+      <button class="dashboard__main__btn" style="backgroundColor:#34AA53">יציאה להפסקה</button>
+      <button class="dashboard__main__btn" style="backgroundColor:#FF3366">סגירת משמרת</button>
+    </main>
   </section>
 </template>
 
