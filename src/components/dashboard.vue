82b9347c5bd7754f6a9008e676144ffd5d37793a
shani kupiec <shani.kupiec@gmail.com>
2022-07-25 16:13:12 +0300

    dashboard functiosn

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -5,31 +5,32 @@
       <customCard>
         <template v-slot:img> <img src="../../public/Union.png" alt="" /> </template>
         <template v-slot:title> תאריך:</template>
-        <template v-slot:data> 2022 </template>
+        <template v-slot:data> {{ new Date().toUTCString() }} </template>
       </customCard>
       <customCard>
         <template v-slot:img> <img src="../../public/logout.png" alt="" /> </template>
         <template v-slot:title> נכנס ב- </template>
-        <template v-slot:data> 2022 </template>
+        <template v-slot:data> {{ startShiftClock }} </template>
       </customCard>
       <customCard>
         <template v-slot:img> <img src="../../public/time.png" alt="" /> </template>
         <template v-slot:title> סה"כ שעות: </template>
-        <template v-slot:data> 2022 </template>
+        <template v-slot:data> {{ hoursOfWork }} </template>
       </customCard>
       <customCard>
         <template v-slot:img> <img src="../../public/coffee.png" alt="" /> </template>
         <template v-slot:title> הפסקה: </template>
-        <template v-slot:data> 2022 </template>
+        <template v-slot:data> {{ hoursOfBreak }} </template>
       </customCard>
       <button @click="startShift()" class="dashboard__main__btn" style="backgroundColor: #5863f8" :class="{ disable: loggedInUser.timeLog[0].end === null }">כניסה למשמרת</button>
-
       <button @click="onBreak()" class="dashboard__main__btn" style="backgroundColor: #34aa53" :class="{ disable: loggedInUser.timeLog[0].end !== null }">
         {{ isAboutToStartBreak ? "יציאה הפסקה" : "סיום להפסקה" }}
       </button>
-
       <button @click="endShift()" class="dashboard__main__btn" style="backgroundColor: #ff3366" :class="{ disable: loggedInUser.timeLog[0].end !== null }">סגירת משמרת</button>
     </main>
+    <section class="dashboard__clock">
+      dashboard__clock
+    </section>
   </section>
   <pre>
     {{ isAboutToStartBreak }}
@@ -58,15 +59,15 @@ export default {
       this.$store.dispatch({ type: "updateUser", user: this.loggedInUser });
     },
     onBreak() {
-      if(this.isAboutToStartBreak){
+      if (this.isAboutToStartBreak) {
         this.loggedInUser.timeLog[0].breaks.unshift({
           start: Date.now(),
-          end: null
-          })
-        console.log('strting break');
-      } else{
-        this.loggedInUser.timeLog[0].breaks[0].end = Date.now()
-        console.log('ending break');
+          end: null,
+        });
+        console.log("strting break");
+      } else {
+        this.loggedInUser.timeLog[0].breaks[0].end = Date.now();
+        console.log("ending break");
       }
       this.$store.dispatch({ type: "updateUser", user: this.loggedInUser });
     },
@@ -82,6 +83,26 @@ export default {
     },
     isAboutToStartBreak() {
       return this.$store.getters.loggedInUser?.timeLog[0].breaks[0]?.end !== null;
+    },
+    startShiftTime() {
+      return this.$store.getters.loggedInUser?.timeLog[0].start;
+    },
+    startShiftClock() {
+      return new Date(this.startShiftTime).getHours() + ":" + new Date(this.startShiftTime).getMinutes() + ":" + new Date(this.startShiftTime).getSeconds();
+    },
+    hoursOfWork() {
+      let millisecondsOfWork = Date.now() - this.startShiftTime - this.timeOfBreak;
+      return (millisecondsOfWork / 1000 / 60 / 60).toFixed(2);
+    },
+    hoursOfBreak() {
+      return (this.timeOfBreak / 1000 / 60 / 60).toFixed(2);
+    },
+    timeOfBreak() {
+      let millisecondsOfBreak = 0;
+      for (let index = 0; index < this.loggedInUser.timeLog[0].breaks.length; index++) {
+        millisecondsOfBreak += this.loggedInUser.timeLog[0].breaks[index].end - this.loggedInUser.timeLog[0].breaks[index].start;
+      }
+      return millisecondsOfBreak;
     },
   },
 };
